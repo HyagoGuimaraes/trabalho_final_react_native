99de02c58cbe07ad080bbc4a1d9716dff0b9afff
Hyago Guimarães <rosah3677@gmail.com>
2025-11-25 00:01:01 -0300

    Alterações em erros

--- src/components/CardLogin/index.tsx
+++ src/components/CardLogin/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Alert, TextInput, View } from "react-native";
-import { useAuth } from "../../auth/useAuth";
+import { useAuth } from '../../auth/useAuth';
 import { ButtonForgot } from "../Button/Forgot";
 import { ButtonLogin } from "../Button/Login";
 import { styles } from "./style";
@@ -23,7 +23,6 @@ export const CardLogin = () => {
       Alert.alert("Bem Vindo!")
       setTimeout(() => { navigation.navigate('StackHome') }, 2000)
     }
-
   };
 
   return (
